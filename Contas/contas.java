0922d8750859175a3033a56c2d3aac54051a70fb
Massato01 <massatohc@gmail.com>
2022-11-11 18:47:04 -0300

    PUTO

--- Contas/contas.java
+++ Contas/contas.java
@@ -1,1 +1,53 @@
-System.out.print("Contas PUTO");
+public class Contas {
+    
+    // VARIÁVEIS
+    private double L = 200e-12;
+    private double h = 6.626e-34;
+    private double m = 9.11e-31;
+    private double c = 3e8;
+    
+    
+    /*
+    FORMULAS
+
+    -> ENERGIA INICIAL E FINAL
+      En = (h² / 8 * m * L²) * n²
+
+      [ n inicial]
+      Ei = (h² / 8 * m * L²) * ni²
+      
+      [ n final ]
+      Ef = (h² / 8 * m * L²) * nf²
+
+      --- ENERGIA DO FOTON
+          E_final - E_inicial
+
+    
+    -> FREQUENCIA
+      E_foton / h
+    
+
+    -> COMPRIMENTO
+      (h * c) / E_foton
+
+
+    -> COMPRIMENTO ELETRON
+       h² / sqrt(2 * m * E)
+
+       --- COMPRIMENTO ELETRON INICIAL
+           h² / sqrt(2 * m * Ei)
+       
+       --- COMPRIMENTO ELETRON FINAL
+           h² / sqrt(2 * m * Ef)
+
+
+    -> VELOCIDADE
+       sqrt((2 * E) / m)
+
+       --- VELOCIDADE INICIAL
+           sqrt((2 * Ei) / m)
+
+       --- VELOCIDADE FINAL
+           sqrt((2 * Ef) / m)
+    */
+}
