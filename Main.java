98b8fc7243a61f2fd2a31bfd9dc465dd24c585cc
Massato01 <massatohc@gmail.com>
2022-11-15 21:26:36 -0300

    PUTO

--- Main.java
+++ Main.java
@@ -1,6 +1,4 @@
 package pkg222210106_carlos_chinen_projeto;
-import java.util.Scanner;
-import pkg222210106_carlos_chinen_projeto.model.Contas;
 
 /**
  *
@@ -9,48 +7,7 @@ import pkg222210106_carlos_chinen_projeto.model.Contas;
 
 class Main {
   public static void main(String[] args) {
-    // ----- Entradas -----
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Digite a largura (L) [m]: ");
-    double L = scanner.nextDouble();
-    
-    System.out.print("Digite ni: ");
-    int ni = scanner.nextInt();
-
-    System.out.print("Digite ni: ");
-    int nf = scanner.nextInt();
-
-    System.out.print("Digite A [m]: ");
-    double a = scanner.nextDouble();
-
-    System.out.print("Digite B [m]: ");
-    double b = scanner.nextDouble();
-    // --------------------
-
-    Contas teste = new Contas(L, ni, nf, a, b);
-
-    // Energia Inicial e Final
-    teste.EnergiaFoton();
-
-    // Frequência
-    teste.FrequenciaFoton();
-
-    // Comprimento
-    teste.ComprimentoFoton();
-
-    // Comprimento de Onda De Broglie
-    teste.ComprimentoDeBroglie();
-
-    // Velocidade
-    teste.Velocidade();
-
-    // Função de Onda
-    teste.FuncaoDeOnda();
-
-    // Probabilidade
-    teste.Probabilidade();
-
-    System.out.print("\nEND");
+    System.out.print("MAIN");
   }
   
 }
---
+++ controller/Controller.java
@@ -0,0 +1,57 @@
+package pkg222210106_carlos_chinen_projeto.controller;
+
+import java.util.Scanner;
+import pkg222210106_carlos_chinen_projeto.model.Contas;
+
+/**
+ *
+ * @author Carlos Massato, Gabriel Missima
+ */
+
+class Controller {
+  public static void main(String[] args) {
+    // ----- Entradas -----
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Digite a largura (L) [m]: ");
+    double L = scanner.nextDouble();
+    
+    System.out.print("Digite ni: ");
+    int ni = scanner.nextInt();
+
+    System.out.print("Digite ni: ");
+    int nf = scanner.nextInt();
+
+    System.out.print("Digite A [m]: ");
+    double a = scanner.nextDouble();
+
+    System.out.print("Digite B [m]: ");
+    double b = scanner.nextDouble();
+    // --------------------
+
+    Contas teste = new Contas(L, ni, nf, a, b);
+
+    // Energia Inicial e Final
+    teste.EnergiaFoton();
+
+    // Frequência
+    teste.FrequenciaFoton();
+
+    // Comprimento
+    teste.ComprimentoFoton();
+
+    // Comprimento de Onda De Broglie
+    teste.ComprimentoDeBroglie();
+
+    // Velocidade
+    teste.Velocidade();
+
+    // Função de Onda
+    teste.FuncaoDeOnda();
+
+    // Probabilidade
+    teste.Probabilidade();
+
+    System.out.print("\nEND");
+  }
+  
+}
